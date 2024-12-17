d6540f838c1528009c5da545f821f4aa1c61e2d8
Austin Polk <austinpolk32@gmail.com>
2024-12-16 23:33:47 -0600

    remove hashing and compression from byte cache, assume that his has been done on the key and content already

--- Code/byte_cache.py
+++ Code/byte_cache.py
@@ -6,29 +6,15 @@ import sys
 # the url will be hashed and used as a search key on the table, which will be sorted by hash value (hashing done with SHA256)
 # the html will be compressed bytes that are decompressed when fetched (compression done thru blosc2)
 class byte_cache:
-    def __init__(self, cache_file: str) -> None:
+    def __init__(self, cache_file: str, cache_index: int) -> None:
         self.index: list[bytes] = []            # index of cache, will be hashes of known urls
         self.content: list[bytes] = []          # cache content, bytes representing compressed html
-
         self.cache_file: str = cache_file       # filepath where contents are saved to and loaded from
+        self.cache_index: int = 0               # index of this cache, appended to cache_file to form a filename
 
-        # constants
-        self._int_width: int = 5
-        self._byteorder: str = sys.byteorder
-        self._hash_width: int = 32
-
-    def _hash_url(self, url: str) -> bytes:
-        return hashlib.sha256(url.encode('utf-8')).digest()
-    
-    def _compress(self, html_content: bytes) -> bytes:
-        return brotli.compress(html_content)
-    
-    def _decompress(self, compressed: bytes) -> bytes:
-        return brotli.decompress(compressed)
-    
-    # find the index of this hash in the table index list or where this hash would be inserted,
-    # return that index and a boolean indicating whether the hash exists at this index
-    def _find_index(self, hashed: bytes) -> int:
+    # find the index of this key in the table index list or where this key would be inserted,
+    # return that index and a boolean indicating whether the key actually exists at this index
+    def _find_index(self, key: bytes) -> int:
         if not self.index:
             return 0, False
 
@@ -38,9 +24,9 @@ class byte_cache:
 
         while L <= R:
             m = mid()
-            if self.index[m] < hashed:
+            if self.index[m] < key:
                 L = m + 1
-            elif self.index[m] > hashed:
+            elif self.index[m] > key:
                 R = m - 1
             else:
                 return m, True
@@ -48,42 +34,37 @@ class byte_cache:
         return L, False
     
     # check if this url has cached html data
-    def is_cached(self, url: str) -> bool:
-        hashed = self._hash_url(url)
-        _, in_cache = self._find_index(hashed)
+    def is_cached(self, key: bytes) -> bool:
+        _, in_cache = self._find_index(key)
         return in_cache
     
     # cache this html_content using the url as a key.
     # will update the content if the url already exists as a key
-    def cache(self, url: str, html_content: bytes) -> None:
-        hashed = self._hash_url(url)
-        compressed = self._compress(html_content)
-        idx, in_cache = self._find_index(hashed)
+    def cache(self, key: bytes, content: bytes) -> None:
+        idx, in_cache = self._find_index(key)
         if in_cache:
-            self.content[idx] = compressed
+            self.content[idx] = content
         else:
-            self.index.insert(idx, hashed)
-            self.content.insert(idx, compressed)
+            self.index.insert(idx, key)
+            self.content.insert(idx, content)
 
     # get a cached value if it exists, otherwise return None
-    def retrieve(self, url: str) -> bytes | None:
-        hashed = self._hash_url(url)
-        idx, in_cache = self._find_index(hashed)
+    def retrieve(self, key: bytes) -> bytes | None:
+        idx, in_cache = self._find_index(key)
         if not in_cache:
             return None
-        compressed = self.content[idx]
-        decompressed = self._decompress(compressed)
-        return decompressed
-    
+        return self.content[idx]
+        
     # save the cache to a binary file
     # file format of saved file:
-    # integer signifying the number of entries in the index (5 byte integer)
-    # bytes of each hash value in the index, in order (32 bytes each)
+    # integer signifying the number of entries in the index
+    # bytes of each hash value in the index, in order
     # for each content:
-    #   integer signifying the length of the compressed content (5 byte integer)
+    #   integer signifying the length of the compressed content
     #   bytes in compressed content (variable byte length)
-    def save(self) -> None:
-        with open(self.cache_file, 'wb+') as f:
+    def save(self, int_width: int, byte_order: str) -> None:
+        filepath = f'{self.cache_file}-{self.cache_index}'
+        with open(filepath, 'wb+') as f:
             index_size = len(self.index)
             index_size_bytes = index_size.to_bytes(self._int_width, self._byteorder)
             f.write(index_size_bytes)
@@ -96,8 +77,9 @@ class byte_cache:
                 f.write(content)
 
     # load the cache from a binary file, following the same spec used in the save method
-    def load(self) -> None:
-        with open(self.cache_file, 'rb') as f:
+    def load(self, index_width: int, int_width: int, byte_order: str) -> None:
+        filepath = f'{self.cache_file}-{self.cache_index}'
+        with open(filepath, 'rb') as f:
             index_size_bytes = f.read(self._int_width)
             index_size = int.from_bytes(index_size_bytes, self._byteorder)
             for _ in range(index_size):
@@ -110,33 +92,25 @@ class byte_cache:
                 self.content.append(content)
 
     # load only the index from a binary file
-    def load_index(self) -> None:
-        with open(self.cache_file, 'rb') as f:
+    def load_index(self, index_width: int, int_width: int, byte_order: str) -> None:
+        filepath = f'{self.cache_file}-{self.cache_index}'
+        with open(filepath, 'rb') as f:
             index_size_bytes = f.read(self._int_width)
             index_size = int.from_bytes(index_size_bytes, self._byteorder)
             for _ in range(index_size):
                 hashed = f.read(self._hash_width)
                 self.index.append(hashed)
-            
-    # clear out this cache from memory by removing references to its members, 
-    # presumably after saving it to persisent storage.
-    def offload(self) -> None:
-        del self.index
-        del self.content
-        pass
 
     # split this cache into two separate caches, distribute half of the elements to each cache,
     # return a tuple of the two new caches
     def split(self) -> tuple:
-        lesser = html_cache()
-        greater = html_cache()
+        lesser = byte_cache(self.cache_file, 2 * self.cache_index)
+        greater = byte_cache(self.cache_file, 2 * self.cache_index + 1)
 
         midpoint = len(self.index) // 2
         lesser.index = self.index[:midpoint]
         lesser.content = self.content[:midpoint]
         greater.index = self.index[midpoint:]
         greater.content = self.index[midpoint:]
-
-        self.offload()
 
         return (lesser, greater)
